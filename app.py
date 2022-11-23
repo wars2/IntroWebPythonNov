100706436fe2073065ce69e78940510bae783c19
Hugo Borges <hugoboareto@gmail.com>
2022-11-23 14:37:00 +0000

    teste com posts fakes

--- app.py
+++ app.py
@@ -1,8 +1,31 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, g
+import sqlite3
+
+DATABASE = "banco.bd"
+SECRET_KEY = "1234"
 
 app = Flask("Olá mundo")
 
-@app.route("/alunos")
+app.config.from_object(__name__)
+
+def conectar():
+    return sqlite3.connect(DATABASE)
+
+@app.before_request
+def before_request():
+    g.bd = conectar()
+
+@app.teardown_request
+def teardown_request(f):
+    g.bd.close()
+
 @app.route("/")
-def alunos():
-    return render_template("hello.html")
+def exibir_posts():
+    sql = "SELECT titulo, texto, data_criacao FROM posts ORDER BY id DESC"
+    resultado = g.bd.execute(sql)
+
+    post =  [
+                {"titulo":"Meu titulo", "texto":"primeiro texto", "data_criacao":"23/11/2022"},
+                {"titulo":"Meu titulo 2", "texto":"segundo texto", "data_criacao":"24/11/2022"}
+            ]
+    return render_template("hello.html", post=post)
